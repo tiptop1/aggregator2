146d7d8f6b52bf876bd56c9a3a8d7cf096b4a540
tiptop <tomasz.gasior@gmail.com>
2026-08-17 23:18:01 +0200

    Fix some syntax errors.

--- src/candle_provider.rs
+++ src/candle_provider.rs
@@ -1,6 +1,6 @@
 use anyhow::{Result, bail, anyhow};
 use reqwest::{Client, StatusCode, Url, header::HeaderMap};
-use rust_decimal::Decimal;
+use rust_decimal::{Decimal, from_str};
 use serde_json_path::JsonPath;
 use serde_json::{Number, Value, from_str};
 use chrono::{DateTime, Utc};
@@ -31,14 +31,14 @@ pub struct HttpCandleProvider {
 
 impl HttpCandleProvider {
     fn to_candles(&self, json_str: &str) -> Result<Vec<Candle>> {
-        let value = from_str(json_str)?;
+        let value = &from_str(json_str)?;
 
         let timestamp_vec = Self::get_timestamps(&self.fields_config.timestamp, value)?;
         let open_vec = Self::get_decimals(&self.fields_config.open, value)?;
         let high_vec = Self::get_decimals(&self.fields_config.high, value)?;
         let low_vec = Self::get_decimals(&self.fields_config.low, value)?;
         let close_vec = Self::get_decimals(&self.fields_config.close, value)?;
-        let volume_vec = self.fields_config.volume.map(|v| Self::get_decimals(v.as_str(), value)?);
+        let volume_vec = self.fields_config.volume.map(|v| Self::get_decimals(v.as_str(), value));
 
         let timestamps_len = timestamp_vec.len();
         let opens_len = open_vec.len();
@@ -47,7 +47,7 @@ impl HttpCandleProvider {
         let closes_len = close_vec.len();
         let volumes_len = volume_vec.map_or(0, |v| v.len());
 
-        if timestamps_len == open_vec && opens_len == highs_len && highs_len == lows_len && lows_len == closes_len && (volumes_len == 0 || (closes_len == volumes_len)) {
+        if timestamps_len == opens_len && opens_len == highs_len && highs_len == lows_len && lows_len == closes_len && (volumes_len == 0 || (closes_len == volumes_len)) {
             let mut candles: Vec<Candle> = Vec::with_capacity(timestamps_len);
             for i in 0..timestamps_len {
                 let candle = Candle {
@@ -62,26 +62,38 @@ impl HttpCandleProvider {
             }
             candles
         } else {
-            bail!("Could not create candles - lengths mismatch (timestamps={}, opens={}, highs={}, lows={}, closes={}{})", timestamps_len, opens_len, highs_len, lows_len, closes_len, if volumes_len == 0 "" else format!(", volumes={}", volumes_len));
+            bail!("Could not create candles - lengths mismatch (timestamps={}, opens={}, highs={}, lows={}, closes={}{})", timestamps_len, opens_len, highs_len, lows_len, closes_len, if volumes_len == 0 {""} else {format!(", volumes={}", volumes_len).as_str()});
         }
     }
 
-    fn get_timestamps(path_str: &str, value: &Value) -> Result<Vec<DataTime<Utc>>> {
+    fn get_timestamps(path_str: &str, value: &Value) -> Result<Vec<DateTime<Utc>>> {
         let path = JsonPath::parse(path_str)?;
         let nodes = path.query(value).all();
-        Ok(nodes.into_iter().map(|v| Self::value_to_timestam(v)?).collect())
+        let mut timestamps: Vec<DateTime<Utc>> = Vec::with_capacity(nodes.len());
+        for n in &nodes {
+            timestamps.push(Self::value_to_timestamp(n)?);
+
+        }
+        Ok(timestamps)
     }
 
     fn get_decimals(path_str: &str, value: &Value) -> Result<Vec<Decimal>> {
-        let path = JsonPath::parse(path_str);
-        let nodes = path.quer(value).all();
-        Ok(nodes.into_iter().map(|v| Self::valute_to_decimal(v)).collect())
+        let path = JsonPath::parse(path_str)?;
+        let nodes = path.query(value).all();
+        Ok(nodes.into_iter().map(|v| Self::value_to_decimal(v)?).collect())
     }
 
     fn value_to_timestamp(value: &Value) -> Result<DateTime<Utc>> {
         match value {
-            Value::Number(millis) => Ok(Self::millis_to_timestamp(millis).unwrap_or_else(|| bail!("Could not convert milliseconds {} to timestamp", millis))),
-            _ => bail!("Could not convert value {} to timestamp", value.as_str())
+            Value::Number(millis) => Self::millis_to_timestamp(millis).map_err(|_| anyhow!("Could not convert milliseconds {} to timestamp", millis)),
+            _ => bail!("Could not convert value {} to timestamp", value.as_str().unwrap_or_else(|| "?"))
+        }
+    }
+
+    fn value_to_decimal(value: &Value) -> Result<Decimal> {
+        match value {
+            Value::Number(number) => Decimal::from_str_exact(&number.to_string()).map_err(|_| anyhow!("Could not convert number {} to decimal", number)),
+            _ => bail!("Could not convert value {} to decimal", value.as_str().unwrap_or_else(|| "?"))
         }
     }
 
@@ -104,7 +116,7 @@ impl CandleProvider for HttpCandleProvider {
         let resp = req_builder.send().await?;
         let resp_status = resp.status();
         let resp_text = resp.text().await?;
-        if resp.status().is_success() {
+        if resp_status.is_success() {
             self.to_candles(&resp_text)
         } else {
             bail!("HTTP status: {}, body: {}", resp_status.as_str(), resp_text);
