70c96d7fbb8bbbd42fa466a604163d64c0ca04bc
tiptop <tomasz.gasior@gmail.com>
2026-09-10 12:42:33 +0200

    Add 'Degug', 'Clone' and 'PartialEq' attribute.

--- src/domain.rs
+++ src/domain.rs
@@ -1,11 +1,12 @@
 use chrono::{DateTime, Utc};
 use rust_decimal::Decimal;
 
+#[derive(Debug, Clone, PartialEq)]
 pub struct Candle {
     pub timestamp: DateTime<Utc>,
     pub open: Decimal,
     pub low: Decimal,
     pub high: Decimal,
     pub close: Decimal,
-    pub volume: Option<Decimal>
+    pub volume: Option<Decimal>,
 }
